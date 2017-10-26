5a884a66a4f6d28ead4a9fe74197680eb8b9598d
eleves <eleves@raphael.local>
2017-10-26 10:57:26 -0400

    v6

--- Raphael_TPFINAL1/AppDelegate.swift
+++ Raphael_TPFINAL1/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        sleep(7)
+        sleep(5)
         return true
     }
 
